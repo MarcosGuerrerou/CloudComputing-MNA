32371d716caa7b0d58557fc036dcdbc915b1bd2b
marcosguerrerou <marquinhogro@gmail.com>
2023-01-25 17:40:37 -0600

    Se añaden comentarios

--- Tarea_1/sumar_arreglos.cpp
+++ Tarea_1/sumar_arreglos.cpp
@@ -1,17 +1,25 @@
+// Inclución de librería OpenMP (Procesamiento paralelo) y IOstream (Input / Output)
 #include <omp.h>
 #include <iostream>
 
+// Definición de constantes para tamaño de los arreglos, tamaño de las divisiones y 
+// cuántos serán mostrados en la consola.
 #define N 1000
 #define chunk 100
 #define mostrar 10
 
+// Declaración de función para imprimir arreglos.
 void imprimeArreglo(float *d);
 
+// Función principal
 int main(){
     std::cout << "Sumando Arreglos en Paralelo! \n";
+    
+    // Declarar arreglos y contador.
     float a[N], b[N], c[N];
     int i;
 
+    // Generación de arreglos a partir de valores pre-definidos.
     for (i=0; i < N; i++){
         a[i] = i*10;
         b[i] = (i+3)*3.7;
@@ -19,14 +27,17 @@ int main(){
 
     int pedazos = chunk;
 
+    // Ciclo paralelo
     #pragma omp parallel for \
     shared(a,b,c, pedazos) private(i) \
     schedule(static, pedazos)
 
+    // Ciclo para realizar operación
     for (i = 0; i < N; i++){
         c[i] = a[i] + b[i];
     }
 
+    // Mostrar resultados
     std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
     imprimeArreglo(a);
     std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
@@ -35,6 +46,7 @@ int main(){
     imprimeArreglo(c);
 }
 
+// Función para imprimir arreglos
 void imprimeArreglo(float *d){
     for (int x = 0; x < mostrar; x++){
         std::cout << d[x] << " - ";
